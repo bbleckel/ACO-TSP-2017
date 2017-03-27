2cb8907eac9d51390c23a37120b279416f9eacf6
jasperhouston <JasperHouston@dhcp-208-67.wireless.bowdoin.edu>
2017-03-27 09:58:55 -0400

    Added algorithm specification to input

--- main.cpp
+++ main.cpp
@@ -8,14 +8,16 @@
 
 void printInfo() {
 	cout << endl;
-	cout << "Usage:\nFor Elite Ant System:\n./main numAnts iterations pheromoneInfluence heuristicInfluence evaporation elitism" << endl;
-	cout << "    numAnts             = number of ants in the colony (int)" << endl;
+	cout << "Usage:\nFor Elite Ant System:\n./main algorithm numAnts iterations pheromoneInfluence heuristicInfluence evaporation elitism" << endl;
+	cout << "    algorithm           = type of algorithm, Elite Ant System or Ant Colony System (eas or acs) (string)" << endl;
+  cout << "    numAnts             = number of ants in the colony (int)" << endl;
 	cout << "    iterations          = number of iterations (int)" << endl;
 	cout << "    pheromoneInfluence  = alpha, the degree of influence of the pheromone component (double):" << endl;
   cout << "    heuristicInfluence  = beta, the degree of influence of the heuristic component (double):" << endl;
   cout << "    evaporation         = rho, the pheromone evaporation factor (double):" << endl;
   cout << "    elitism             = e, the elitism factor (double):" << endl;
-  cout << "Usage:\nFor Ant Colony System:\n./main numAnts iterations pheromoneInfluence heuristicInfluence evaporation epsilon tau0 q0" << endl;
+  cout << "Usage:\nFor Ant Colony System:\n./main algorithm numAnts iterations pheromoneInfluence heuristicInfluence evaporation epsilon tau0 q0" << endl;
+  cout << "    algorithm           = type of algorithm, Elite Ant System or Ant Colony System (eas or acs) (string)" << endl;
 	cout << "    numAnts             = number of ants in the colony (int)" << endl;
 	cout << "    iterations          = number of iterations (int)" << endl;
 	cout << "    pheromoneInfluence  = alpha, the degree of influence of the pheromone component (double):" << endl;
@@ -23,13 +25,13 @@ void printInfo() {
   cout << "    evaporation         = rho, the pheromone evaporation factor (double):" << endl;
   cout << "    epsilon             = factor controlling wearing away of pheromones (double):" << endl;
   cout << "    tau0                = factor controlling wearing away of pheromones (double):" << endl;
-  cout << "    q0                  = the probability in Ant Colony System that an ant will choose the best leg for the next leg of the tour it is constructing, instead of choosing probabilistically (double):" << endl;
+  cout << "    q0                  = probability that an ant will choose the best leg for the next leg of the tour it is constructing, instead of choosing probabilistically (double)" << endl;
 	cout << endl;
 }
 
 
 int main (int argc, char** argv) {
-  
+
 
 
 
