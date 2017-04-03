273eac1f2472210761518e35e1db959bb8fa9f03
Dylan H. Parsons <dylan.parsons1@gmail.com>
2017-04-03 12:36:51 -0400

    File reading

--- ACO.cpp
+++ ACO.cpp
@@ -1,3 +1,77 @@
 #include "ACO.h"
 
 using namespace std;
+
+void printPoint(point2D p) {
+    cout << "(" << p.x << ", " << p.y << ")" << endl;
+    }
+
+ACOSolver::ACOSolver(string fileName) {
+    // constructor
+    this->fileName = fileName;
+    readFile();
+}
+
+ACOSolver::~ACOSolver() {
+    // destructor
+}
+
+void ACOSolver::readFile() {
+    // read file
+    string line;
+    ifstream inputFile;
+    inputFile.open(fileName, ios::in);
+    
+    int x, y;
+    point2D p;
+    
+    int started = 0;
+    if(!inputFile.is_open()) {
+        cerr << "ERROR: Could not open file" << endl;
+        exit(1);
+    } else {
+        while(getline(inputFile, line)) {
+            if (line.front() == '1') {
+                started = 1;
+                string entry;
+                string delimiter = " ";
+                
+                // save coordinates
+                x = stof(line.substr(0, line.find(delimiter)));
+                line.erase(0, line.find(delimiter) + delimiter.length());
+                y = stof(line.substr(0, line.find(delimiter)));
+                line.erase(0, line.find(delimiter) + delimiter.length());
+                
+                p.x = x;
+                p.y = y;
+                cities.push_back(p);
+                
+            } else if (line.front() == 'E' && line.back() == 'F') {
+                // done
+                break;
+            } else if (started) {
+                string entry;
+                string delimiter = " ";
+                
+                x = stof(line.substr(0, line.find(delimiter)));
+                line.erase(0, line.find(delimiter) + delimiter.length());
+                y = stof(line.substr(0, line.find(delimiter)));
+                line.erase(0, line.find(delimiter) + delimiter.length());
+                
+                p.x = x;
+                p.y = y;
+                cities.push_back(p);
+                
+            }
+        }
+        inputFile.close();
+    }
+    cout << "Printing cities!" << endl;
+    for(int i = 0; i < cities.size(); i++) {
+        printPoint(cities[i]);
+    }
+}
+
+
+
+// render cities in OpenGL for lulz?
---
+++ ACO.h
@@ -0,0 +1,35 @@
+#ifndef ACO_H
+#define ACO_H
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+typedef struct _point2d {
+    double x, y;
+} point2D;
+
+void printPoint(point2D p);
+
+class Ant {
+    
+};
+
+class ACOSolver {
+public:
+    vector<point2D> cities;
+    ACOSolver(string fileName);
+    ~ACOSolver();
+    
+    string fileName;
+private:
+    void readFile();
+};
+
+
+
+#endif
